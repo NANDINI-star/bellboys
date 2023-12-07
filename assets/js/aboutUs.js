6034cb5e05043f94c4068cfbfce880036651622d
nandini bagga <nandinibagga@192.168.1.6>
2023-12-08 03:51:53 +0530

    improved about us text animation

--- assets/js/aboutUs.js
+++ assets/js/aboutUs.js
@@ -1,4 +1,20 @@
-window.addEventListener('scroll', () => {
-  console.log("scrolling")
-  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+var descs = document.querySelectorAll(".au-desc");
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "body",
+    scrub: 1,
+    end: "50%",
+    pin: true,
+
+  }
+});
+
+descs.forEach((element, i) => {
+  tl.from(".clr", {color: "white", fontStyle: "normal"})
+  tl.to(element, {
+    y: -461*i,
+    duration: 3
+  });
+  
+  tl.to(".clr", {color: "#FFEB5B", duration: 3, fontStyle: "italic"});
+});
