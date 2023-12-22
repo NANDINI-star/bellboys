77790449fa4d729b860ecf816575623ed0182373
nandini bagga <nandinibagga@nandinis-MacBook-Air.local>
2023-12-22 19:09:41 +0530

    fixed our-work

--- assets/js/ourWork.js
+++ assets/js/ourWork.js
@@ -13,6 +13,7 @@ const wrap = gsap.utils.wrap(0, sections.length);
 const goTo = (index, direction = 'first') => {
   console.log(index, sections[0], txts[0], imgs[0]);
   index = wrap(index);
+  console.log(index);
   animating = true;
   let leave, enter;
   const defaultPos = { transform: 'none', autoAlpha: 1 };
@@ -39,6 +40,7 @@ const goTo = (index, direction = 'first') => {
   // tl.fromTo(imgs[index], enter, defaultPos);
   
   if(currentIndex > -1) {
+    console.log("idhr", currentIndex, direction);
     tl
       .to(sections[currentIndex], leave, 0)
       .set(sections[currentIndex], { transform: 'none' });
@@ -60,7 +62,7 @@ Observer.create({
   target: ".ow-content",
   wheelSpeed: -1,
   tolerance: 20,
-  onDown: () => !animating && goTo(currentIndex - 1, 'up'),
+  // onDown: () => !animating && goTo(currentIndex - 1, 'up'),
   onUp: () => !animating && goTo(currentIndex + 1, 'down' ),
   preventDefault: true,
 })
