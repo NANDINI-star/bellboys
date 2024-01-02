5d3a4bff50bbfe9e9f8b9118b5eb97b27a41fda0
nandini bagga <nandinibagga@nandinis-MacBook-Air.local>
2024-01-02 20:04:14 +0530

    about us

--- assets/js/aboutUs.js
+++ assets/js/aboutUs.js
@@ -1,23 +1,102 @@
-ScrollTrigger.defaults({
-  // toggleActions: "restart pause resume pause",
-  scroller: ".about-us-desc"
-})
+// ScrollTrigger.defaults({
+//   // toggleActions: "restart pause resume pause",
+//   scroller: ".about-us-desc"
+// })
 
-gsap.fromTo(".clr-1", {color: 'white'},{
-  scrollTrigger: ".ad-1",
-  color: "#FFEB5B",
-  delay: 1
-})
-gsap.fromTo(".clr-2", {color: 'white'},{
-  scrollTrigger: ".ad-2",
-  color: "#FFEB5B",
-  delay: 1
-})
-gsap.fromTo(".clr-3", {color: 'white'},{
-  scrollTrigger: ".ad-3",
-  color: "#FFEB5B",
-  delay: 1
-})
+// function scrollNext(){
+//   console.log("scrollNext")
+//   gsap.fromTo(".ad-0",{y:0}, {
+//     y:"-50vh"
+//   })
+//   gsap.fromTo(".ad-1", {y:0},{y: "-50vh"})
+//   gsap.fromTo(".clr-1", {color: 'white'},{
+//     // scrollTrigger: ".ad-1",
+//     color: "#FFEB5B",
+//     delay: 1
+//   })
+//   gsap.to(".ad-2", {y: "-100vh", delay: 1.5}, "start1")
+//   gsap.fromTo(".clr-2", {color: 'white'},{
+//     // scrollTrigger: ".ad-2",
+//     color: "#FFEB5B",
+//     delay: 2
+//   }, "start1")
+//   gsap.to(".ad-3", {y: "-150vh", delay: 2.5}, "start2")
+//   gsap.fromTo(".clr-3", {color: 'white'},{
+//     // scrollTrigger: ".ad-3",
+//     color: "#FFEB5B",
+//     delay: 3
+//   }, "start2")
+//   // gsap.to(".ad-3", {y: "50vh", delay: 3.5}, "start3")
+//   // gsap.fromTo(".clr-3", {color: 'white'},{
+//   //   // scrollTrigger: ".ad-3",
+//   //   color: "#FFEB5B",
+//   //   delay: 4
+//   // }, "start3")
+//   // gsap.to(".ad-2", {y: "100vh", delay: 4.5}, "start4")
+//   // gsap.fromTo(".clr-2", {color: 'white'},{
+//   //   // scrollTrigger: ".ad-2",
+//   //   color: "#FFEB5B",
+//   //   delay: 5
+//   // }, "start4")
+//   // gsap.to(".ad-1", {y: "150vh", delay: 5.5}, "start5")
+//   // gsap.fromTo(".clr-1", {color: 'white'},{
+//   //   // scrollTrigger: ".ad-1",
+//   //   color: "#FFEB5B",
+//   //   delay: 6
+//   // }, "start5")
+// }
+
+// // gsap.to(".ad-0", {
+// //   y:"-50vh"
+// // })
+// // gsap.to(".ad-1", {y: "-50vh"})
+// // gsap.fromTo(".clr-1", {color: 'white'},{
+// //   // scrollTrigger: ".ad-1",
+// //   color: "#FFEB5B",
+// //   delay: 1
+// // })
+// // gsap.to(".ad-2", {y: "-100vh", delay: 1.5}, "start1")
+// // gsap.fromTo(".clr-2", {color: 'white'},{
+// //   // scrollTrigger: ".ad-2",
+// //   color: "#FFEB5B",
+// //   delay: 2
+// // }, "start1")
+// // gsap.to(".ad-3", {y: "-150vh", delay: 2.5}, "start2")
+// // gsap.fromTo(".clr-3", {color: 'white'},{
+// //   // scrollTrigger: ".ad-3",
+// //   color: "#FFEB5B",
+// //   delay: 3
+// // }, "start2")
+// // gsap.to(".ad-3", {y: "50vh", delay: 3.5}, "start3")
+// // gsap.fromTo(".clr-3", {color: 'white'},{
+// //   // scrollTrigger: ".ad-3",
+// //   color: "#FFEB5B",
+// //   delay: 4
+// // }, "start3")
+// // gsap.to(".ad-2", {y: "100vh", delay: 4.5}, "start4")
+// // gsap.fromTo(".clr-2", {color: 'white'},{
+// //   // scrollTrigger: ".ad-2",
+// //   color: "#FFEB5B",
+// //   delay: 5
+// // }, "start4")
+// scrollNext();
+// setInterval(scrollNext, 10000);
+
+// gsap.fromTo(".clr-1", {color: 'white'},{
+//   scrollTrigger: ".ad-1",
+//   color: "#FFEB5B",
+//   delay: 1
+// })
+// gsap.fromTo(".clr-2", {color: 'white'},{
+//   scrollTrigger: ".ad-2",
+//   color: "#FFEB5B",
+//   delay: 1
+// })
+// gsap.fromTo(".clr-3", {color: 'white'},{
+//   scrollTrigger: ".ad-3",
+//   color: "#FFEB5B",
+//   delay: 1
+// })
 
 
 
